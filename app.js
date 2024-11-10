2d6cd32c8608b9f6c14323b590486313dc2a6159
20058340 <20058340@mydbs.ie>
2024-11-10 23:41:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// app.js
 const express = require('express');
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Barath!');
+  res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://54.74.245.12:8080/);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Example app listening at http://localhost:${port}/`);
 });
